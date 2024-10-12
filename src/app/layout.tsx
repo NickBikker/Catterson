4caecaf5efebb4c3e4ff6a11b2dc2f9aea12a89f
Nick Bikker <nbikker@kleyn.com>
2024-10-12 18:33:12 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css';
+
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Kleyn Dev - NextJs Starter',
+  description: 'Maintained by Kleyn Dev. Generated from base NextJS',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
